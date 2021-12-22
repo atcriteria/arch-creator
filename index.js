9396b3851c6b86e9a98f3a44580d21c2cb6bfdde
dude lao <brycelipscomb@gmail.com>
2021-12-22 01:20:03 -0500

    some more styling, have the app saving text files as well as arch files

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // The Arch Creator code
 
-const container = document.getElementById("app-container");
-container.innerText = "Hello World"
+const downloadToFile = () => {
+    console.log("called save")
+    // const container = document.getElementById('test-content');
+    const container = document.querySelector("textarea");
+    const content = container.value;
+    const filename = "test.txt";
+    const contentType = "text/plain";
+    const a = document.createElement('a');
+    const file = new Blob([content], {type: contentType});
+
+    a.href = URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+}
