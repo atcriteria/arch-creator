10575d572d0af5321d06d31320350d35b4bc629e
dude lao <brycelipscomb@gmail.com>
2021-12-22 03:22:11 -0500

    updated a style, added state management for the weapon form and a custom hook to process these changes

--- src/components/WeaponForm.js
+++ src/components/WeaponForm.js
@@ -1,20 +1,36 @@
-import downloadToFile from "../util/downloadFile"
+import downloadToFile from "../util/downloadFile";
+import { useState } from "react";
+
+const initialValues = {
+    name: "",
+    image: "",
+    type: "Axe",
+
+}
 
 export default function WeaponForm(){
+    const [state, setState] = useState(initialValues);
+
+    const handleChange = e => {
+        e.preventDefault();
+        return setState({...state,
+        [e.target.name]: e.target.value})
+    }
+
     return(
         <div className="arch-form-container">
             <form>
               <div>
                   <p>Name:</p>
-                  <input name="name" type="text" placeholder="name" />
+                  <input name="name" type="text" placeholder="name" value={state.name} onChange={handleChange} />
               </div>
               <div>
-                  <p>Image Path:</p>
-                  <input name="image-path" type="text" placeholder="format/like/this.png"/>
+                  <p>Image Name:</p>
+                  <input name="image" type="text" placeholder={`weapons/crafting/${(state.type).toLowerCase()}/<this-is-what-youre-changing>`}/>
               </div>
               <div>
                   <p>Weapon Type:</p>
-                  <select name="weapon-type" id="weapon-type">
+                  <select name="type" id="weapon-type" onChange={handleChange} >
                       <option value="Axe">Axe</option>
                       <option value="Blade">Blade</option>
                       <option value="Bow">Bow</option>
